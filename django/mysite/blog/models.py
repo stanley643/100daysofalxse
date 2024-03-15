ce7fa7308afd6a1ebd9863afae13721099a9f915
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-15 22:47:30 +0300

    100daysofcode

--- django/mysite/blog/models.py
+++ django/mysite/blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models herei.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                    .filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -21,6 +25,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
