fe1f5029c7423b3795948a7a0db0e9088ce8d89c
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-16 20:44:56 +0300

    100daysofcode

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 # Create your views here.
@@ -9,11 +9,9 @@ def post_list(request):
                   {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(Post,
+                            id=id,
+                            status=Post.Status.PUBLISHED)
     
     return render(request,
                   'blog/post/detail.html',
