5ee6da30b327a97fe867c287f625be2bc1e0fadd
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-17 20:04:11 +0300

    100daysofcode

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,4 +1,4 @@
-from calendar import day_abbr, month, year
+from calendar import Day, month
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
@@ -15,7 +15,7 @@ def post_detail(request, id):
                             slug=post,
                             publish__year=year,
                             publish__month=month,
-                            publish__day=day_abbr)
+                            publish__day=Day)
     
     return render(request,
                   'blog/post/detail.html',
