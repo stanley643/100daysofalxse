62288ce4fa0b1e50438c303590578395c7b42a25
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-21 21:39:27 +0300

    100daysofcode

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator
+from django.views.generic import ListView
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
 
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    #paginator = Paginator(post_list, 3)
+    #page_number = request.GET.get('page', 1)
+    #posts = paginator.page(page_number)
     
 
     return render(request,
@@ -26,3 +27,11 @@ def post_detail(request, id):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+class PostListView(ListView):
+    """Alternative post list view"""
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
