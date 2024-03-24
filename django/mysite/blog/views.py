5d331b420502e64731d4fb505c7da3a8cdd54cfa
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-24 19:32:15 +0300

    100daysofcode

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -76,7 +76,7 @@ def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
 
-    form = CommentForm(data=requested.POST)
+    form = CommentForm(data=request.POST)
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
