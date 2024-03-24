2f6e81ffed30a38d38f0e615e5c9c764640ec933
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-24 19:30:09 +0300

    100daysofcode

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -73,7 +73,7 @@ def post_share(request, post_id):
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_objectr_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
 
     form = CommentForm(data=requested.POST)
