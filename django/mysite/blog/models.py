9a25dea75964c1b2ec7b1cedca0fe89d528e2c1c
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-25 20:00:35 +0300

    code a code

--- django/mysite/blog/models.py
+++ django/mysite/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
 # Create your models herei.
 
 class PublishedManager(models.Manager):
@@ -30,6 +32,7 @@ class Post(models.Model):
     
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
