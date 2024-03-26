bddeb4806f1fb1f8019ace3b981c2479fe8b319e
Stanley643 <nmurigi@kabarak.ac.ke>
2024-03-26 23:32:25 +0300

    code a code

--- django/mysite/blog/urls.py
+++ django/mysite/blog/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('<int:post_id>/comment/',
         views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/',
-        views.PostListView, name='post_list_by_tag'),
+        views.post_list, name='post_list_by_tag'),
 ]
