ee767bf94b7282fc3cdd346ff04e763d98260eaf
Stanley643 <nmurigi@kabarak.ac.ke>
2024-04-14 20:54:34 +0300

    unittests

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertFalse('FOO'.isupper())
 
     def test_split(self):
         s = 'hello world'
